91be9e107d2c6153edd8d338d0a840312e467b35
bitam <mazloombita@foothill.edu>
2020-10-02 07:09:08 -0700

    Initial course material

---
+++ src/game/GuessOrSink.java
@@ -0,0 +1,69 @@
+package game;
+
+// imports the Scanner class to use for reading from System.in
+import java.util.Scanner;
+
+/**
+ * A game in which the goal is for the user to guess a the two letter
+ * abbreviation of the fifty states. If the user guesses correctly the
+ * application will show the ASCII representation of a ship. Otherwise,
+ * it will show a sunken ship.
+ * 
+ * @author Foothill College, [YOUR NAME HERE]
+ */
+public class GuessOrSink 
+{
+	/**
+	 * Stores a 2-letter word for the user to guess. Then, prompts the user to enter 
+	 * a single letter for their guess. Draws an ASCII representation of a 
+	 * ship depending on whether the user guessed correctly.
+	 * @param args    not used
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO: Define the necessary variables.
+		
+		
+		// TODO: Define the booleans and initialize them to false.
+		
+		
+		// TODO: Define and initialize the variables that represent the ship.
+		// Note: Each variable should represent one row (i.e. line) of the ASCII representation of the ship;
+		//       Each variable should represent one row (i.e. line) of the ASCII representation of the water;
+		//       In a similar manner create additional variables to represent the different parts of the scene.
+
+		// TODO: Define the various ship representations. For example: full ship, 
+		//       partially sunken ship, to sunken ship.
+		
+		// TODO: Select a 2 letter state to guess	
+
+		// TODO: Create an object of type Scanner to read user input.
+
+				
+		// Let's ROCK! Starting with introduction to the game.
+		// TODO: Provide a welcome message.
+
+		// TODO: Prompt user for the first guess.
+		
+		// TODO: Use the Scanner object to read the next word.
+		
+		// TODO: Store the first character in a variable called guess1.
+		
+		// TODO: Check if guess1 is correct.
+		//       If true, show the first initial, and set your boolean to true.
+		//       Otherwise, ask for a second guess.
+		
+		// TODO: If a second guess is necessary, check it.
+		//       If the second guesses is also incorrect, redraw the ship sinking.		
+
+		
+		// TODO: Prompt the user to guess the second initial. 
+		// TODO: Repeat the steps (you took for the first initial).
+		//       However, this time if the second guess is incorrect, show the sunken ship.
+		//       Otherwise, show the saved ship.
+	
+
+		// TODO: Let the user know what the word is and provide an ending message
+				
+	} 
+}
